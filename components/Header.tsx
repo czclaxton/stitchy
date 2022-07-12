367c3a6b38e564dd2695d9400fce740a2119e163
czclaxton <czclaxton@gmail.com>
2022-07-12 12:15:05 -0400

    initial styling for header component on the home page

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 const Header = () => {
   return (
-    <header>
+    <header className='flex justify-between p-5 max-w-7xl mx-auto'>
       <div className='flex items-center space-x-5'>
         <Link href='/'>
           <img
@@ -14,10 +14,17 @@ const Header = () => {
         <div className='hidden md:inline-flex items-center space-x-5'>
           <h3>About</h3>
           <h3>Contact</h3>
-          <h3>Follow</h3>
-        </div>{' '}
+          <h3 className='text-white bg-green-600 px-4 py-1 rounded-full'>
+            Follow
+          </h3>
+        </div>
       </div>
-      <div></div>
+      <div className='flex items-center space-x-5 text-green-600'>
+        <h3>Sign In</h3>
+        <h3 className='border px-4 py-1 rounded-full border-green-600'>
+          Get Started
+        </h3>
+      </div>
     </header>
   )
 }
