6fe96b0a7199f313b50e4753417593c3171b2bc5
czclaxton <czclaxton@gmail.com>
2022-07-12 14:13:46 -0400

    implemented ssr and posts for home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,15 @@
-import type { NextPage } from 'next'
+import Link from 'next/link'
 import Head from 'next/head'
 import Header from '../components/Header'
+import { sanityClient, urlFor } from '../sanity'
+import { Post } from '../typings'
 
-const Home: NextPage = () => {
+interface Props {
+  posts: [Post]
+  children: Element
+}
+
+const Home = ({ posts }: Props) => {
   return (
     <div className='mx-auto'>
       <Head>
@@ -34,12 +41,64 @@ const Home: NextPage = () => {
               alt='logo'
             />
           </div>
-
-          {/* POSTS HERE */}
         </div>
+      </div>
+
+      <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-6 p-2 md:p-6'>
+        {posts?.map(post => (
+          <Link key={post._id} href={`/post/${post.slug.current}`}>
+            <div className='border rounded group cursor-pointer overflow-hidden'>
+              {post.mainImage && (
+                <img
+                  className='h-60 w-full object-cover rounded-t group-hover:scale-105 transition-transform duration 200 ease-in-out'
+                  src={urlFor(post.mainImage).url()}
+                  alt=''
+                />
+              )}
+              <div className='flex justify-between p-5 bg-white'>
+                <div>
+                  <p className='text-lg font-bold'>{post.title}</p>
+                  <p className='text-xs'>
+                    {post.description} by {post.author.name}
+                  </p>
+                </div>
+
+                {post.author?.image && (
+                  <img
+                    className='h-12 w-12 rounded-full'
+                    src={urlFor(post.author.image).url()}
+                    alt='author image'
+                  />
+                )}
+              </div>
+            </div>
+          </Link>
+        ))}
       </div>
     </div>
   )
 }
 
 export default Home
+
+export const getServerSideProps = async () => {
+  const query = `*[_type == "post"]{
+    _id,
+    title,
+    author -> {
+    name, 
+    image
+  }, 
+    description,
+    mainImage,
+    slug
+  }`
+
+  const posts = await sanityClient.fetch(query)
+
+  return {
+    props: {
+      posts,
+    },
+  }
+}
--- sanity.js
+++ sanity.js
@@ -1,12 +1,18 @@
-import {
-  createImageUrlBuilder,
-  createCurrentUserHook,
-  createClient,
-} from 'next-sanity'
+import { createCurrentUserHook, createClient } from 'next-sanity'
+import createImageUrlBuilder from '@sanity/image-url'
 
 export const config = {
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  apiVersion: '',
+  apiVersion: '2021-03-25',
   useCdn: process.env.NODE_ENV === 'production',
 }
+
+// Used to fetch info, make queries, etc
+export const sanityClient = createClient(config)
+
+// Helper fn for generate image urls
+export const urlFor = source => createImageUrlBuilder(config).image(source)
+
+// Helper fn for accessing the current logged in user
+export const useCurrentUser = createCurrentUserHook(config)
