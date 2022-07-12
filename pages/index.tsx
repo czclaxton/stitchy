addaed49ba8de474605cbbaa7d5f8e23bc7e4c06
czclaxton <czclaxton@gmail.com>
2022-07-12 11:06:33 -0400

    initialized project

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Home: NextPage = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <Head>
+        <title>Stitchy Blog</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <h1>This is Stitchy</h1>
+    </div>
+  )
+}
+
+export default Home
